0cab7671cd006aa2f814a3f4b4ac4461259342ac
Edmond GAKS <edmondgaks@gmail.com>
2022-03-03 15:56:18 +0200

    the php for validating the other forms and for form validation

---
+++ signup.php
@@ -0,0 +1,36 @@
+<?php
+
+if(isset($_POST['submit'])) {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $findus = $_POST['findus'];
+    $news = $_POST['news'];
+    $message = $_POST['message'];
+    
+    
+    if(($name=="") || ($email=="")) {
+        echo "Please provide all information";
+    }
+    
+    else {
+        define("HOST","localhost");
+        // echo "I have full details";
+        $DB_host = "localhost";
+        $DB_user = "root";
+        $DB_password = "";
+        $DB_name = "omnifood";
+        $connection = mysqli_connect(HOST,$DB_user,$DB_password,$DB_name);
+    }
+    
+    
+    if(!$connection) {
+        echo "Connection error";
+    } else {
+        $insertQuery= "INSERT INTO clients(name,email,findus,news,message) values('$name','$email','$findus','$news','$message')";
+        $insert = mysqli_query($connection,$insertQuery) or die("Error occured".mysqli_error($connection));
+        if($insert) {
+            echo "DATA added Successfully";
+        }
+    }
+}
