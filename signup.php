5b6428b5065938d3f9cf2afb63be8280be4241ba
Edmond GAKS <edmondgaks@gmail.com>
2022-03-04 11:30:31 +0200

    adding html page in php

--- signup.php
+++ signup.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <?php
 
 if(isset($_POST['submit'])) {
@@ -33,4 +43,10 @@ if(isset($_POST['submit'])) {
             echo "DATA added Successfully";
         }
     }
+
 }
+?>
+
+<a href="index.html">RETURN TO THE MAIN PAGE</a>
+</body>
+</html>
